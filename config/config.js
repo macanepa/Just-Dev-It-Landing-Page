426e555c8f1e771d296d947a60f0638957e2a49b
Joaquín Espíldora <jespildora1@gmail.com>
2024-12-23 20:51:06 -0300

    Se crea documento config.js

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    cal: {
+        apiKey: 'cal_live_346c329ace218cce3e93195cd481b2b4',
+        calLink: 'just-dev-it/30min',
+        webhookUrl: 'tu_webhook_url'
+    }
+};
+
+export default config;
